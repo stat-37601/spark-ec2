c2e363c4c717ff2e57778469691fb494329c0e2d
Jeremy Archer <fatlotus@gmail.com>
2015-03-31 22:13:20 -0500

    Re-add numpy reinstall.

--- setup-slave.sh
+++ setup-slave.sh
@@ -130,7 +130,6 @@ chmod a+x /usr/bin/realpath
 popd > /dev/null
 
 # Ensure that numpy is installed properly on every machine.
-# pip-2.7 install -U --force-reinstall numpy
-# Disabled by Jeremy Archer on 31 Mar 2015.
+pip-2.7 install -U --force-reinstall numpy
 
 ((nohup sleep 10800 && shutdown -h now) &) &
